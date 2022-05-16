580bd9c0713a4efdc0fecdd883fd201a9f4c0869
emeraldhycient <igwezehycient86@gmail.com>
2022-05-16 19:57:33 +0100

    [update] added clearer error message

--- src/controllers/todo/addtodo.js
+++ src/controllers/todo/addtodo.js
@@ -4,7 +4,7 @@ const addTodo = (req, res) => {
   const { title, description, status } = req.body;
 
   if (!title || !description || !status) {
-    return res.status(404).json({ notfound: "pls enter all todos" });
+    return res.status(422).json({ notfound: "pls enter all todos" });
   } else {
     const newTodo = new todo({
       title: title,
@@ -14,12 +14,12 @@ const addTodo = (req, res) => {
     newTodo
       .save()
       .then((todo) => {
-        res
+        return res
           .status(200)
           .json({ status: "success", message: "todo added", todo: todo });
       })
       .catch((err) => {
-        res.status(422).json({
+        res.status(500).json({
           status: "failed",
           message: "internal serve error",
           todo: {
