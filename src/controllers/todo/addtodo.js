3389e5ac0c0413f6c65368030629d25cab900426
emeraldhycient <igwezehycient86@gmail.com>
2022-05-16 19:43:09 +0100

    deploying

--- src/controllers/todo/addtodo.js
+++ src/controllers/todo/addtodo.js
@@ -1,9 +1,9 @@
 const todo = require("../../models/todo/todo");
 
 const addTodo = (req, res) => {
-  const { title, description, status, category } = req.body;
+  const { title, description, status } = req.body;
 
-  if (!title || !description || !status || !category) {
+  if (!title || !description || !status) {
     return res.status(404).json({ notfound: "No todos found" });
   } else {
     const newTodo = new todo({
@@ -19,9 +19,15 @@ const addTodo = (req, res) => {
           .json({ status: "success", message: "todo added", todo: todo });
       })
       .catch((err) => {
-        res
-          .status(422)
-          .json({ status: "failed", message: "pls enter all the fields" });
+        res.status(422).json({
+          status: "failed",
+          message: "pls enter all the fields",
+          todo: {
+            title: title,
+            description: description,
+            status: status,
+          },
+        });
       });
   }
 };
